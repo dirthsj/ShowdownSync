bce76d381401d74d6463e759b698b87f9c522dd6
KingofGamesYami <steven.dirth@gmail.com>
2016-09-04 19:59:09 -0500

    Updated to make more effecient

--- content_script.js
+++ content_script.js
@@ -1,14 +1,10 @@
-chrome.runtime.sendMessage({greeting: "get"}, function(response) {
-	console.log( response );
+chrome.storage.sync.get( 'showdown_teams_sync', function( response ){
 	window.localStorage.setItem( 'showdown_teams_local', response.showdown_teams_sync );
-	console.log( "got sync data" );
-});
-console.log( "waiting for sync data" );
+	console.log( 'got sync data, set localStorage' );
+	console.log( response.showdown_teams_sync );
+})
 
 window.onbeforeunload = function(){
 	var team = window.localStorage.getItem( 'showdown_teams_local' );
-	console.log( 'saving teams' );
-	console.log( team );
-	chrome.runtime.sendMessage( {greeting: "set", showdown_teams_local: team} );
-	return '';
+	chrome.storage.sync.set( {showdown_teams_sync: team } );
 }
